71d9e80ee6724c772b1a5794ba90e5b055938c38
lyesuper <871879832@qq.com>
2022-04-27 15:16:06 +0000

    more changes

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,5 +1,4 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
-
+from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv, combine_codelists, filter_codes_by_category # NOQA
 
 study = StudyDefinition(
     default_expectations={
@@ -7,7 +6,47 @@ study = StudyDefinition(
         "rate": "uniform",
         "incidence": 0.5,
     },
-    population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
+    population=patients.all(),
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.51}},
+        }
+    ),
+    age=patients.age_as_of(
+        "2019-09-01",
+        return_expectations={
+            "rate": "universal",
+            "int": {"distribution": "population_ages"},
+        },
+    ),
+    bmi=patients.most_recent_bmi(
+        between=["2010-02-01", "2020-01-31"],
+        minimum_age_at_measurement=18,
+        include_measurement_date=True,
+        date_format="YYYY-MM",
+        return_expectations={
+            "date": {"earliest": "2010-02-01", "latest": "2020-01-31"},
+            "float": {"distribution": "normal", "mean": 28, "stddev": 8},
+            "incidence": 0.80,
+        }
+    ),
+    gp_count=patients.with_gp_consultations(
+        between=["2019-01-01", "2020-12-31"],
+        returning="number_of_matches_in_period",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 6, "stddev": 3},
+            "incidence": 0.6,
+        },
+    ),
+    flu_vaccine=patients.with_tpp_vaccination_record(
+        target_disease_matches="influenza",
+        between=["2019-09-01", "2020-04-01"],
+        returning="date",
+        date_format="YYYY-MM",
+        find_first_match_in_period=True,
+        return_expectations={
+            "date": {"earliest": "2019-09-01", "latest": "2020-03-29"}
+        }
     ),
 )
